fbc224cde5319471c5b85e130ecd0d56812c5d53
Sergio Gómez <sgomezguti@uoc.edu>
2026-03-05 17:24:21 +0100

    Corrección de código (aparecían errores al compilar)

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,41 +1,21 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
-import { CommonModule } from '@angular/common';
-
-import { DetailComponent } from './components/detail/detail'; 
+import { DetailComponent } from './components/detail/detail';
 
 @Component({
   selector: 'app-root',
-  standalone: true, 
-  imports: [RouterOutlet, CommonModule, DetailComponent],
-=======
-
-@Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
->>>>>>> 380690c6f4d5c3eaa9838ff6fc5fb61f686f3dac
+  standalone: true,
+  imports: [RouterOutlet, DetailComponent],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrls: ['./app.css']
 })
-export class App {
-  protected readonly title = signal('equipo-basket');
-<<<<<<< HEAD
+export class AppComponent {
+  title = 'equipo-basket';
 
-  public jugadorSeleccionado = signal<any>(null);
-
-  constructor() {
-    // ESTO ES SOLO PARA PROBAR: 
-    this.jugadorSeleccionado.set({
-      nombre: 'Pau',
-      apellidos: 'Gasol',
-      posicion: 'Pívot',
-      edad: 43,
-      altura: 2.16,
-      foto: 'PauGasol.avif'
-    });
-  }
+  jugadorSeleccionado = {
+    nombre: 'Pau Gasol',
+    posicion: 'Pívot',
+    altura: '2.16m',
+    foto: 'PauGasol.avif'
+  };
 }
-=======
-}
->>>>>>> 380690c6f4d5c3eaa9838ff6fc5fb61f686f3dac
--- src/app/components/detail/detail.ts
+++ src/app/components/detail/detail.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core'; 
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 @Component({
@@ -6,8 +6,8 @@ import { CommonModule } from '@angular/common';
   standalone: true,
   imports: [CommonModule],
   templateUrl: './detail.html',
-  styleUrl: './detail.css'
+  styleUrls: ['./detail.css']
 })
 export class DetailComponent {
-  @Input() jugador: any = null;
+  @Input() jugador: any;
 }
