29b3e3a61435e8729985067046cf23819af2d9e1
ChampCTE <celia.trulla@gmail.com>
2026-03-13 16:07:35 +0100

    Creación Arquetipos con *ngFor

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { PlayersComponent } from './components/players/players';
+import { ArquetiposComponent } from './components/arquetipos/arquetipos';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'players', component: PlayersComponent }
+  { path: 'players', component: PlayersComponent },
+  { path: 'arquetipos', component: ArquetiposComponent }
 ];
