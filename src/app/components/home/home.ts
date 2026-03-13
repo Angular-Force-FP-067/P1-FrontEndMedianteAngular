8b2c56b4ec5882e1faf87346b747552199152b2d
Sergio Gómez <sgomezguti@uoc.edu>
2026-03-13 15:43:48 +0100

    Elimino Pau Gasol de la home y añado detalles a todos los jugadores.

--- src/app/components/home/home.ts
+++ src/app/components/home/home.ts
@@ -1,25 +1,14 @@
 import { Component } from '@angular/core';
-import { DetailComponent } from '../detail/detail';
-import { Media } from '../media/media';
+import { PlayersComponent } from '../players/players';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [DetailComponent, Media],
+  imports: [PlayersComponent],
   templateUrl: './home.html',
   styleUrls: ['./home.css']
 })
 export class HomeComponent {
   title = 'Equipo Basket';
   subtitle = 'Plantilla 2026';
-
-  jugadorSeleccionado = {
-    nombre: 'Pau',
-    apellidos: 'Gasol',
-    posicion: 'Pívot',
-    altura: '2.16',
-    edad: 43,
-    foto: 'PauGasol2.avif',
-    video: 'assets/videos/jugador01.mp4'
-  };
 }
