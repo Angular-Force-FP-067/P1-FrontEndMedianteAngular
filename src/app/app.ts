19d211252633436b95e30a320bc9f3765140d96b
helenavivas <helenavivas@uoc.edu>
2026-03-13 18:59:46 +0100

    fixes banner

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,18 +1,30 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Router, RouterOutlet, NavigationEnd } from '@angular/router';
+import { filter } from 'rxjs';
 import { HeaderComponent } from './shared/header/header.component';
 import { FooterComponent } from './shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [
-    RouterOutlet,
-    HeaderComponent,
-    FooterComponent
-  ],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrls: ['./app.css']
 })
 export class AppComponent {
+  isLanding = false;
+
+  constructor(private router: Router) {
+    this.updateLayout(this.router.url);
+
+    this.router.events
+      .pipe(filter((event) => event instanceof NavigationEnd))
+      .subscribe((event: NavigationEnd) => {
+        this.updateLayout(event.urlAfterRedirects);
+      });
+  }
+
+  private updateLayout(url: string): void {
+    this.isLanding = url === '/' || url === '';
+  }
 }
